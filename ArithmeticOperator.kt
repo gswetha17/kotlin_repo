901dfdeffcdceb6f0621352d5b566f4916ef6f63
gswetha17 <147020331+gswetha17@users.noreply.github.com>
2023-10-18 11:36:11 +0530

    Update ArithmeticOperator.kt
    your code is greate but i have added some code

--- ArithmeticOperator.kt
+++ ArithmeticOperator.kt
@@ -3,8 +3,12 @@ package com.lkrd.chatgpt
 fun main() {
     var number1:Double=8.4
     var number2:Double=4.2
+    var number3:Double=3.4
     var result:Double=0.0
 
+    result=number2 + number3
+    println("number2 + number3=" + result)
+    
     result= number1 + number2
 
     println("number1 + number2=" + result)
@@ -24,4 +28,7 @@ fun main() {
     result= number1 % number2
 
     println("number1 % number2=" + result)
+
+
+    println("Congratulation i have solve")
 }
